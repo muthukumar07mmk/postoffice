c6313ac25a709e764ea5d573193cf975ed15d157
kiran-exalogic <kiran@exalogic.in>
2018-03-31 08:06:18 +0530

    Bug

--- dbConfig.php
+++ dbConfig.php
@@ -1,9 +1,8 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$user="root";
-$pwd="";
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $user, $pwd, $db);
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$server = $url["host"];
+	$user="root";
+	$pwd="";
+	$db = substr($url["path"], 1);
+	$conn=new mysqli($server,$user,$pwd,$db);
 ?>
